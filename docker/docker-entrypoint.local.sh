ec7793b07377d2c4b4530dc59726934816e97803
Kamal Lama <kamal09rumba@gmail.com>
2020-03-08 11:47:33 +0545

    WIP

---
+++ docker/docker-entrypoint.local.sh
@@ -0,0 +1,5 @@
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py compilemessages -l ne -l en
+python manage.py migrate --no-input
+python manage.py runserver 0.0.0.0:8000
